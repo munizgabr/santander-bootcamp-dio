d12a948c25bbd7f50e9884e983b225774bdcdda5
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2021-07-16 13:11:12 -0300

    criando projeto - exemplo Threads da API Java (novos arquivos)

---
+++ API-thread-JAVA/src/test/java/com/oneinnovationdigital/APIthreadJAVA/ApiThreadJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneinnovationdigital.APIthreadJAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiThreadJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
